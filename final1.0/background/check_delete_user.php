db32116a4fdb5f002f354272cca537cd84f944cb
尹俊民 <howdoesmylove@qq.com>
2014-11-07 00:36:03 +0800

    转移到 1.0去了一些

---
+++ final1.0/background/check_delete_user.php
@@ -0,0 +1,28 @@
+<?php
+	/*链接数据库*/
+	header("Content-Type:text/html; charset=utf-8");
+ 	$link=mysql_connect("localhost","root","676792")or die("数据库服务器连接错误".mysql_error());
+    mysql_select_db("test",$link) or die("数据库访问错误".mysql_error());    //选择数据库
+    mysql_query("set names utf8");     
+    $id = $_GET['id'];                                       //选择字符集标准格式
+    $sql1=mysql_query("select * from user where uid=$id");
+    $row1=mysql_fetch_object($sql1);
+     $filename=$row1->photo;
+     $flag = ($filename == "default.jpg");
+    $filename = "../bookfun/Public/Uploads/".$filename;
+    if (file_exists($filename)) { 
+    	if(!$flag)
+    		unlink($filename); 
+    }
+	mysql_query("SET FOREIGN_KEY_CHECKS = 0");
+	$sql=mysql_query("delete from collection where uid=$id");
+	$sql=mysql_query("delete from comment where uid=$id");
+	$sql=mysql_query("delete from user where uid=$id");//删除信息
+	if($sql){	//删除成功跳转
+	echo "<script>alert('信息删除成功！');history.back();window.location.href=document.referrer;</script>";
+	}
+	else{		//删除失败跳转
+	echo "<script>alert('信息删除失败！');history.back();window.location.href=document.referrer;</script>";
+	}
+	?>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
