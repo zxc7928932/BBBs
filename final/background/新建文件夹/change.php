b5befabb1c148fa49fd2b2e651f84ae0a15dd368
尹俊民 <howdoesmylove@qq.com>
2014-11-07 00:34:59 +0800

    级联删除 修改@陈豪

---
+++ final/background/新建文件夹/change.php
@@ -0,0 +1,143 @@
+<?php 
+	session_start();
+	header("Content-Type:text/html;charset=utf-8");
+	if($_SESSION['member']==""){
+	echo "<script>alert('请先登录！');history.back();window.location.href=main.php;</script>";
+}
+	$filename1 = $_POST['filename1'];
+	$filename2 = $_POST['filename2'];
+	$filename3 = $_POST['filename3'];
+	$path="../bookfun/Public/v/img/";//设置轮播器路径
+	if($_FILES["filename1"]["name"]!=""){			//如果文件名不为空则图片一需要修改
+	$name1=$_FILES["filename1"]["name"];
+	$end1=strstr($name1,'.');						//截取后缀名
+	
+	$_FILES["filename1"]["name"]="main1.jpg";		//上传图片重命名
+	/*$tp = array("image/gif","image/pjpeg","image/jpeg","image/png"); 
+
+	if(!in_array($_FILES["filename1"]["type"],$tp)) 	//如果格式不正确 跳转
+	{ 
+		echo "<script> alert('图片1请上传正确格式');window.location.href='index2.php';</script>";
+}*/
+	
+	if($_FILES["filename1"]["name"]) 
+	{ 
+		$file1=$_FILES["filename1"]["name"]; 
+		$file2 = $path.$file1; 
+		$flag=1; 
+	} 
+
+	$uploadedfile = $_FILES['filename1']['tmp_name'];
+	if($end1==".jpg"||$end1==".jpeg"){		//判断格式建图
+	$src = imagecreatefromjpeg($uploadedfile);
+}else if($end1==".gif"){
+	$src = imagecreatefromgif($uploadedfile);
+}else{
+	$src = imagecreatefrompng($uploadedfile);
+}
+	list($width,$height)=getimagesize($uploadedfile);
+	$newwidth=750;
+	$newheight=308;
+	$tmp=imagecreatetruecolor($newwidth,$newheight);//建缓存图片
+
+	imagecopyresampled($tmp,$src,0,0,0,0,$newwidth,$newheight,$width,$height); 
+	$filename = $file2;
+	if($end1==".jpg"||$end1==".jpeg"){		//生成图片
+	imagejpeg($tmp,$filename,100);
+}else if($end1==".gif"){
+	imagegif($tmp,$filename,100);
+}else{
+	imagepng($tmp,$filename,9);
+}
+	imagedestroy($src);
+	imagedestroy($tmp); 
+}
+
+if($_FILES["filename2"]["name"]!=""){			//如果文件名不为空则图片二需要修改
+	$name1=$_FILES["filename2"]["name"];
+	$end1=strstr($name1,'.');					//截取后缀名
+
+	$_FILES["filename2"]["name"]="main2.jpg";	//上传图片重命名
+	/*$tp = array("image/gif","image/pjpeg","image/jpeg","image/png"); 
+
+	if(!in_array($_FILES["filename2"]["type"],$tp)) //如果格式不正确 跳转
+	{ 
+		echo "<script> alert('图片1请上传正确格式');window.location.href='index2.php';</script>";
+}*/
+	
+	if($_FILES["filename2"]["name"]) 
+	{ 
+		$file1=$_FILES["filename2"]["name"]; 
+		$file2 = $path.$file1; 
+		$flag=1; 
+	} 
+	
+	$uploadedfile = $_FILES['filename2']['tmp_name'];
+	if(($end1==".jpg")||($end1==".jpeg")){		//判断格式建图
+	$src = imagecreatefromjpeg($uploadedfile);
+}else if($end1==".gif"){
+	$src = imagecreatefromgif($uploadedfile);
+}else{
+	$src = imagecreatefrompng($uploadedfile);
+}
+	list($width,$height)=getimagesize($uploadedfile);
+	$newwidth=750;
+	$newheight=308;
+	$tmp=imagecreatetruecolor($newwidth,$newheight);//建缓存图片
+
+	imagecopyresampled($tmp,$src,0,0,0,0,$newwidth,$newheight,$width,$height); 
+	$filename = $file2;
+	if($end1==".jpg"||$end1==".jpeg"){ 				//生成图片
+	imagejpeg($tmp,$filename,100);
+}else if($end1==".gif"){
+	imagegif($tmp,$filename,100);
+}else{
+	imagepng($tmp,$filename,9);
+}
+	imagedestroy($src);
+	imagedestroy($tmp); 
+}
+
+if($_FILES["filename3"]["name"]!=""){				//如果文件名不为空则图片三需要修改
+	$name1=$_FILES["filename3"]["name"];	
+	$end1=strstr($name1,'.');						//截取后缀名
+	$_FILES["filename3"]["name"]="main3.jpg";		//上传图片重命名
+	/*$tp = array("image/gif","image/pjpeg","image/jpeg","image/png"); //如果格式不正确 跳转
+	{ 
+		echo "<script> alert('图片1请上传正确格式');window.location.href='index2.php';</script>";
+}*/
+	
+	if($_FILES["filename3"]["name"]) 
+	{ 
+		$file1=$_FILES["filename3"]["name"]; 
+		$file2 = $path.$file1; 
+		$flag=1; 
+	} 
+
+	$uploadedfile = $_FILES['filename3']['tmp_name'];
+	if(($end1==".jpg")||($end1==".jpeg")){			//判断格式建图
+	$src = imagecreatefromjpeg($uploadedfile);
+}else if($end1==".gif"){
+	$src = imagecreatefromgif($uploadedfile);
+}else{
+	$src = imagecreatefrompng($uploadedfile);
+}
+	list($width,$height)=getimagesize($uploadedfile);
+	$newwidth=750;
+	$newheight=308;
+	$tmp=imagecreatetruecolor($newwidth,$newheight);		//建缓存图片
+
+	imagecopyresampled($tmp,$src,0,0,0,0,$newwidth,$newheight,$width,$height); 
+	$filename = $file2;
+	if($end1==".jpg"||$end1==".jpeg"){				//生成图片
+	imagejpeg($tmp,$filename,100);
+}else if($end1==".gif"){
+	imagegif($tmp,$filename,100);
+}else{
+	imagepng($tmp,$filename,9);
+}
+	imagedestroy($src);
+	imagedestroy($tmp); 
+}
+	echo "<script> alert('修改成功！');window.location.href='index2.php';</script>";
+ ?>
---
+++ final/background/新建文件夹/index2.php
@@ -0,0 +1,82 @@
+﻿<?php 
+	session_start();
+	if($_SESSION['member']==""){
+	echo "<script>alert('请先登录！');history.back();window.location.href=main.php;</script>";
+}
+?>
+<html>
+<head>
+<title>后台管理</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<table width="828" height="522" border="0" align="center" cellpadding="0" cellspacing="0" id="__01">
+	<tr>
+		<td background="images/image_01.gif">&nbsp;			</td>
+
+		<td height="140" background="images/image_02.gif">&nbsp;			</td>
+	</tr>
+	<tr>
+		<td width="202" rowspan="3" valign="top"><table width="202" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td><?php include("menu.php");?></td>
+          </tr>
+        </table></td>
+		<td height="34" background="images/image_04.gif" style="background-repeat :no-repeat">
+
+			<a href="logout.php" style="margin-left:280px;">退出登录</a>
+			
+		</td>
+		
+	</tr>
+	<tr>
+
+		<td height="38" background="images/image_06.gif">&nbsp;			</td>
+	</tr>
+	
+		
+		<tr>
+		<td height="70" valign="top">
+		  <table width="626" border="0" cellpadding="0" cellspacing="0" >
+			
+              <tr>
+                <td height="237" align="center" valign="top" background="images/image_08.gif">
+       <table style="height:360px;" width="600" height="237"  border="0" cellpadding="0" cellspacing="0">
+       	<form action="change.php" method="post" enctype="multipart/form-data">
+                 <tr>
+          <tr>
+		    <td style="text-align:center;"><h3 style="color:red;">首页轮播器图片修改(gif,jpg,png)</h3></td>
+		   </tr>
+		  <tr>
+		    <td><p style="margin-left:150px;">图片1：</p><input type="file"  name="filename1" style="margin:10px 0px 10px 150px;"> 
+		    </td>
+		  </tr>
+		  <tr>
+		    <td><p style="margin-left:150px;">图片2：</p><input type="file"  name="filename2" style="margin:10px 0px 10px 150px;"></td>
+		  </tr>
+		  <tr>
+		    <td><p style="margin-left:150px;">图片3：</p><input type="file"  name="filename3" style="margin:0px 0px 20px 150px;"></td>
+		  </tr>
+		</tr>
+		<tr>
+		  <td colspan="2" >
+                 
+		  <input type="submit" value="提交" style="margin-left:190px;"/>
+                  
+		  <input type="reset" value="重置" style="margin-left:50px;"/>
+		  </td >
+		</tr>
+              
+                  </form>
+                </table></td>
+              </tr>
+            </table>			</td>
+	</tr>
+	<tr>
+		<td bgcolor="#F0F0F0"></td>
+		<td height="43" background="images/image_12.gif"></td>
+	</tr>
+</table>
+</body>
+</html>
